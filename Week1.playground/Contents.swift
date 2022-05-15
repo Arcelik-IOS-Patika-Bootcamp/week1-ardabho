7745910c156037dea585db30d3f69ab32907985f
Arda Büyükhatipoğlu <ardabho@gmail.com>
2022-05-15 20:37:13 +0300

    Array and Sets practice. Created an optional watchBrands array and used optional binding to print its items if they are not nil.
    Also created two arrays named guitarBrands and sameGuitarBrands which contains the same elements. Then appended them. By turning this string into a Set named guitarBrandsSet I was able to get rid of the duplicate elements.
    Finally, i removed some items from the guitarBrand Set.

--- Week1.playground/Contents.swift
+++ Week1.playground/Contents.swift
@@ -1,3 +1,50 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Array and optionals practice
+
+var watchBrands: [String]?
+var brandString: String = ""
+
+watchBrands = ["Apple", "Seiko", "Wainer", "Patek Philippe", "Casio", "Samsung"]
+
+if let safeBrands = watchBrands {
+    
+    for brand in safeBrands {
+        brandString = brandString + brand + ", "
+    }
+    print("Current brands are: \(brandString)")
+    
+} else {
+    print("Error: brands array is nil")
+}
+
+//----------------------------------------------------------------------------------------------
+// Set Practice
+
+//Adding the same items to an array before turning it into a set
+var guitarBrands = ["Gibson", "Fender", "Cort", "Takamine", "Taylor"]
+var sameGuitarBrands = guitarBrands
+
+guitarBrands.append(contentsOf: sameGuitarBrands)
+
+print("GuitarBrands array before turning it into a Set: \(guitarBrands)")
+
+var guitarBrandsSet = Set(guitarBrands)
+print("GuitarBrands array after turning it into a Set: \(guitarBrandsSet)")
+
+//Removing some items from the set
+guitarBrandsSet.remove("Gibson")
+guitarBrandsSet.remove("Taylor")
+
+guitarBrandsSet
+//---------------------------------------------------------------------------------------------
+//Dictionary practice
+
+var prices: [String : Float] = [:]
+prices = [
+    "Iphone 13" : 24000,
+    "S22 Ultra" : 29000,
+    "Pringles"  : 24,
+    "Butter"    : 25,
+    "Book"      : 60
+]
